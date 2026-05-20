c32b8640592667215241297cfdfa3ff369135560
Wriky <wangruish@hotmail.com>
2026-05-20 11:42:03 +0800

    Initial Commit

---
+++ HotelDemo/HotelDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelDemoApp.swift
+//  HotelDemo
+//
+//  Created by Riky Wang on 2026/5/20.
+//
+
+import SwiftUI
+
+@main
+struct HotelDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
